6034cd1ca6f71db45042f34ed490fcb0ae93cca2
mihyunInst <cmhInst@gmail.com>
2022-11-23 17:11:30 +0900

    First commit

---
+++ src/edu/kh/git/pratice/GitPracticeRun.java
@@ -0,0 +1,10 @@
+package edu.kh.git.pratice;
+
+public class GitPracticeRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("호잇");
+	}
+
+}
