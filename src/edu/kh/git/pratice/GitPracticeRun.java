ea51fe6088a7084cc3127ebf934e4ffac6bb0763
mihyun <cmhInst@gmail.com>
2022-12-05 19:43:44 +0900

    신기하다!

--- src/edu/kh/git/pratice/GitPracticeRun.java
+++ src/edu/kh/git/pratice/GitPracticeRun.java
@@ -5,6 +5,7 @@ public class GitPracticeRun {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("호잇");
+		System.out.println("여러분 안녕하세요");
 	}
 
 }
